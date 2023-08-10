0099ad5c29d449f5c22936f3af58ac2adf6b5236
Pedro Mothsin <pedromothsin@gmail.com>
2023-08-10 04:59:39 -0300

    cleaned the code up

--- app.js
+++ app.js
@@ -1,67 +1,61 @@
-const port = process.env.PORT || 5050
+const port = process.env.PORT || 5050;
 
-const bodyParser = require('body-parser')
-const express = require('express')
-var logger = require('morgan');
-const server = express()
-const cors = require('cors')
+const bodyParser = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
-server.use(logger('dev'));
-server.use(cors())
-server.use( bodyParser.urlencoded({extended: true}) )
-server.use( bodyParser.json() )
+const app = express();
 
-server.listen(port, function(){
-    console.log("Listening on port: "+ port)
-})
+// Middleware
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-server.get("/", function(req, res){
-    res.send("OK")
-})
+// Routes
+app.listen(port, () => {
+  console.log("Listening on port: " + port);
+});
 
-server.post("/test", function(req, res){
-    console.log(req.body)
-    res.send("OK")
-})
-
-server.post("/generate-svg", function(req, res){
-    console.log(req.body)
+app.post("/generate-svg", (req, res) => {
+  try {
     const echarts = require('echarts');
 
-    // In SSR mode the first container parameter is not required
     const chart = echarts.init(null, null, {
-      renderer: 'svg', // must use SVG rendering mode
-      ssr: true, // enable SSR
-      width: 400, // need to specify height and width
+      renderer: 'svg',
+      ssr: true,
+      width: 400,
       height: 300
     });
 
-    // use setOption as normal
     chart.setOption(req.body.option);
 
-    // Output a string
     const svgStr = chart.renderToSVGString();
 
-    res.send(svgStr)
-})
+    res.send(svgStr);
+  } catch (error) {
+    console.error("Error generating SVG:", error);
+    res.status(500).send("Error generating SVG");
+  }
+});
 
-server.post("/generate", function(req, res){
-  console.log(req.body)
-  const echarts = require('echarts');
-  const { createCanvas } = require('canvas');
+app.post("/generate", (req, res) => {
+  try {
+    const echarts = require('echarts');
+    const { createCanvas } = require('canvas');
 
+    const canvas = createCanvas(800, 600);
+    const chart = echarts.init(canvas);
 
-  const canvas = createCanvas(800, 600);
-// ECharts can use the Canvas instance created by node-canvas as a container directly
-  const chart = echarts.init(canvas);
+    chart.setOption(req.body.option);
 
-  // use setOption as normal
-  chart.setOption(req.body.option);
-
-  // Output a string
-  res.writeHead(200, {
-    'Content-Type': 'image/png'
-  });
-  res.write(canvas.toBuffer('image/png'));
-  res.end();
-})
+    res.writeHead(200, {
+      'Content-Type': 'image/png'
+    });
+    res.end(canvas.toBuffer('image/png'));
+  } catch (error) {
+    console.error("Error generating image:", error);
+    res.status(500).send("Error generating image");
+  }
+});
